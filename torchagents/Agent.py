0b7f212756c83293be6d693ac48008949d0e2733
Michael Munje <michael.munje.919@my.csun.edu>
2020-06-20 23:53:27 -0700

    Add method for getting optimal action and distribution.

--- torchagents/Agent.py
+++ torchagents/Agent.py
@@ -1,8 +1,9 @@
 import torch
 from torch.utils.tensorboard import SummaryWriter
 from typing import Tuple
-from .Policy import Policy
-from .utilities import ReplayBuffer
+import numpy as np
+from torchagents import Policy
+from torchagents.utilities import ReplayBuffer
 
 
 class Agent:
@@ -22,10 +23,13 @@ class Agent:
         if off_policy:
             self._buffer = ReplayBuffer(buffer_size, state_shape)
 
-    def get_action(self, state: torch.Tensor):
+    def get_action(self, state: torch.Tensor) -> torch.Tensor:
         if self._off_policy:
             if torch.rand(1) < self._epsilon:
                 return torch.randint(high=self._num_actions, size=[1])
+        return torch.argmax(self.get_action_distribution(state))
+
+    def get_action_distribution(self, state: torch.Tensor) -> torch.Tensor:
         return self._policy.get_action(state)
 
     def update(self):

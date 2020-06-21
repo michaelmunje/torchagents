71f2e48f3612a5b4a044bb26801794573c7d0f3c
Michael Munje <michael.munje.919@my.csun.edu>
2020-06-20 21:11:32 -0700

    Replay buffer automatically uses first gpu if available.

--- test/test_ReplayBuffer.py
+++ test/test_ReplayBuffer.py
@@ -5,38 +5,43 @@ import torch
 
 class TestReplayBuffer(TestCase):
     def test_update(self):
+
+        device = torch.device("cpu")
+        if torch.cuda.is_available():
+            device = torch.device("cuda:0")
+
         # experience: state, action, reward, next state
         buffer = ReplayBuffer(buffer_size=2, state_shape=(1, 2))
 
-        state1 = torch.tensor([[5.0, 10.0]], dtype=torch.float32)
-        action1 = torch.tensor([3])
-        reward1 = torch.tensor([5.0])
-        next_state1 = torch.tensor([[2.0, 3.0]])
+        state1 = torch.tensor([[5.0, 10.0]], dtype=torch.float32, device=device)
+        action1 = torch.tensor([3], device=device)
+        reward1 = torch.tensor([5.0], device=device)
+        next_state1 = torch.tensor([[2.0, 3.0]], device=device)
         buffer.update(state=state1, action=action1,
                       reward=reward1, next_state=next_state1)
 
-        state2 = torch.tensor([[10.0, 15.0]], dtype=torch.float32)
-        action2 = torch.tensor([2])
-        reward2 = torch.tensor([3.0])
-        next_state2 = torch.tensor([[55.0, 22.0]])
+        state2 = torch.tensor([[10.0, 15.0]], dtype=torch.float32, device=device)
+        action2 = torch.tensor([2], device=device)
+        reward2 = torch.tensor([3.0], device=device)
+        next_state2 = torch.tensor([[55.0, 22.0]], device=device)
         buffer.update(state=state2, action=action2,
                       reward=reward2, next_state=next_state2)
 
         (s, a, r, ns) = buffer.get_random_batch(2)
 
-        self.assertTrue(torch.eq(s[0], state1).all().numpy()
-                        or torch.eq(s[0], state2).all().numpy())
+        self.assertTrue(torch.eq(s[0], state1).all().cpu().numpy()
+                        or torch.eq(s[0], state2).all().cpu().numpy())
 
-        self.assertFalse(torch.eq(s[0], action1).all().numpy())
-        self.assertFalse(torch.eq(s[0], next_state1).all().numpy())
+        self.assertFalse(torch.eq(s[0], action1).all().cpu().numpy())
+        self.assertFalse(torch.eq(s[0], next_state1).all().cpu().numpy())
 
-        self.assertTrue(torch.eq(a[0], action1).all().numpy()
-                        or torch.eq(a[0], action2).all().numpy())
+        self.assertTrue(torch.eq(a[0], action1).all().cpu().numpy()
+                        or torch.eq(a[0], action2).all().cpu().numpy())
 
-        self.assertTrue(torch.eq(r[0], reward1).all().numpy()
-                        or torch.eq(r[0], reward2).all().numpy())
+        self.assertTrue(torch.eq(r[0], reward1).all().cpu().numpy()
+                        or torch.eq(r[0], reward2).all().cpu().numpy())
 
-        self.assertTrue(torch.eq(ns[0], next_state1).all().numpy()
-                        or torch.eq(ns[0], next_state2).all().numpy())
+        self.assertTrue(torch.eq(ns[0], next_state1).all().cpu().numpy()
+                        or torch.eq(ns[0], next_state2).all().cpu().numpy())
 
         buffer.reset()
--- torchagents/utilities/ReplayBuffer.py
+++ torchagents/utilities/ReplayBuffer.py
@@ -4,12 +4,19 @@ from typing import Tuple
 
 class ReplayBuffer:
     def __init__(self, buffer_size: int, state_shape: Tuple):
+
+        device = torch.device("cpu")
+        if torch.cuda.is_available():
+            device = torch.device("cuda:0")
+
         # Experience consists of:
         # state, action, reward, nextState
-        self.states = torch.zeros(size=[buffer_size, *state_shape], dtype=torch.float32)
-        self.actions = torch.zeros(size=[buffer_size], dtype=torch.int)
-        self.rewards = torch.zeros(size=[buffer_size], dtype=torch.float32)
-        self.next_states = torch.zeros(size=[buffer_size, *state_shape], dtype=torch.float32)
+        self.states = torch.zeros(size=[buffer_size, *state_shape],
+                                  dtype=torch.float32, device=device)
+        self.actions = torch.zeros(size=[buffer_size], dtype=torch.int, device=device)
+        self.rewards = torch.zeros(size=[buffer_size], dtype=torch.float32, device=device)
+        self.next_states = torch.zeros(size=[buffer_size, *state_shape],
+                                       dtype=torch.float32, device=device)
         self.current_index = 0
         self.buffer_size = buffer_size
 
@@ -22,7 +29,8 @@ class ReplayBuffer:
         self.current_index += 1
         self.current_index %= self.buffer_size
 
-    def get_random_batch(self, batch_size: int):
+    def get_random_batch(self, batch_size: int) -> (torch.Tensor, torch.Tensor,
+                                                    torch.Tensor, torch.Tensor):
         # rand_idx = torch.randint(self.buffer_size, (batch_size,)) without replacement
         rand_idx = torch.randperm(batch_size)
         return (self.states[rand_idx], self.actions[rand_idx],

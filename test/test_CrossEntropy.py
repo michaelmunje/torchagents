5eef324b6eda9b48f153fb99558f556bdce3a89b
Michael Munje <michael.munje.919@my.csun.edu>
2020-06-20 23:53:43 -0700

    Add work in progress for cross entropy

---
+++ test/test_CrossEntropy.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from torchagents import Agent
+from torchagents.algorithms import CrossEntropy
+
+
+class TestCrossEntropy(TestCase):
+    def test_cross_entropy(self):
+        self.fail()
+        # Cartpole will be used here

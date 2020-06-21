3013dd15a5d52297893d1acd393a9f57faccc280
Michael Munje <michael.munje.919@my.csun.edu>
2020-06-20 21:16:55 -0700

    Hide attributes.

--- torchagents/utilities/ReplayBuffer.py
+++ torchagents/utilities/ReplayBuffer.py
@@ -11,31 +11,31 @@ class ReplayBuffer:
 
         # Experience consists of:
         # state, action, reward, nextState
-        self.states = torch.zeros(size=[buffer_size, *state_shape],
-                                  dtype=torch.float32, device=device)
-        self.actions = torch.zeros(size=[buffer_size], dtype=torch.int, device=device)
-        self.rewards = torch.zeros(size=[buffer_size], dtype=torch.float32, device=device)
-        self.next_states = torch.zeros(size=[buffer_size, *state_shape],
-                                       dtype=torch.float32, device=device)
-        self.current_index = 0
+        self._states = torch.zeros(size=[buffer_size, *state_shape],
+                                   dtype=torch.float32, device=device)
+        self._actions = torch.zeros(size=[buffer_size], dtype=torch.int, device=device)
+        self._rewards = torch.zeros(size=[buffer_size], dtype=torch.float32, device=device)
+        self._next_states = torch.zeros(size=[buffer_size, *state_shape],
+                                        dtype=torch.float32, device=device)
+        self._current_index = 0
         self.buffer_size = buffer_size
 
     def update(self, state: torch.Tensor, action: torch.Tensor,
                reward: torch.Tensor, next_state: torch.Tensor) -> None:
-        self.states[self.current_index] = state
-        self.actions[self.current_index] = action
-        self.rewards[self.current_index] = reward
-        self.next_states[self.current_index] = next_state
-        self.current_index += 1
-        self.current_index %= self.buffer_size
+        self._states[self._current_index] = state
+        self._actions[self._current_index] = action
+        self._rewards[self._current_index] = reward
+        self._next_states[self._current_index] = next_state
+        self._current_index += 1
+        self._current_index %= self.buffer_size
 
     def get_random_batch(self, batch_size: int) -> (torch.Tensor, torch.Tensor,
                                                     torch.Tensor, torch.Tensor):
         # rand_idx = torch.randint(self.buffer_size, (batch_size,)) without replacement
         rand_idx = torch.randperm(batch_size)
-        return (self.states[rand_idx], self.actions[rand_idx],
-                self.rewards[rand_idx], self.next_states[rand_idx])
+        return (self._states[rand_idx], self._actions[rand_idx],
+                self._rewards[rand_idx], self._next_states[rand_idx])
 
     def reset(self) -> None:
         # This function has no real use right now
-        self.current_index = 0
+        self._current_index = 0

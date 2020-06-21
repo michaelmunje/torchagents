8ad29061fb997761cd5756ae617a73c0820f3d3b
Michael Munje <michael.munje.919@my.csun.edu>
2020-06-21 01:58:29 -0700

    Can now set agent to off/on policy.

--- torchagents/Agent.py
+++ torchagents/Agent.py
@@ -18,13 +18,13 @@ class Agent:
         self._state_shape = state_shape
         self._num_actions = num_actions
         self._epsilon = epsilon
-        self._off_policy = off_policy
+        self.off_policy = off_policy
 
         if off_policy:
             self._buffer = ReplayBuffer(buffer_size, state_shape)
 
     def get_action(self, state: torch.Tensor) -> torch.Tensor:
-        if self._off_policy:
+        if self.off_policy:
             if torch.rand(1) < self._epsilon:
                 return torch.randint(high=self._num_actions, size=[1])
         return torch.argmax(self.get_action_distribution(state))
